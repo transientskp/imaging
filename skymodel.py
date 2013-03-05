bf9cb3f210360a386267dc916cfefe8f2e1cd536
John Swinbank <swinbank@transientskp.org>
2013-03-05 19:49:05 +0100

    Updated skymodel.py

--- skymodel.py
+++ skymodel.py
@@ -5,13 +5,15 @@ import math
 from pyrap.tables import table
 
 # One from each beam
-important_subbands = ["000", "034", "068", "102", "136", "170", "204"]
+important_subbands = ["000", "040", "080", "120", "160", "200"]
 positions = []
 
-for sb in important_subbands:
-    for msname in glob.glob(os.path.join(sys.argv[1], "*/L*SB%s_uv.MS.dppp" % (sb,))):
-        t = table("%s::FIELD" % (msname,))
-        positions.append(map(math.degrees, t.getcol("REFERENCE_DIR")[0][0]))
+for arg in sys.argv[1:]:
+    for sb in important_subbands:
+        for msname in glob.glob(os.path.join(arg, "L*SB%s_uv.MS.dppp" % (sb,))):
+            t = table("%s::FIELD" % (msname,))
+            positions.append(map(math.degrees, t.getcol("REFERENCE_DIR")[0][0]))
 
-positions = list(set("gsm.py %.2f_%.2f.skymodel %f %f 5 0.1" % (posn[0], posn[1], posn[0], posn[1]) for posn in positions))
-print "\n".join(positions)
+#print positions
+for posn in positions:
+    print "gsm.py %.2f_%.2f.skymodel %f %f 5 0.1" % (posn[0], posn[1], posn[0], posn[1])
